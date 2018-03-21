badcf28d14af3c6a6491e7be6cc75e6a13e9e636
FlorinSenoner <florin.senoner@gmail.com>
2018-03-22 00:08:30 +0100

    reset form after submission

--- src/screens/Invite/index.js
+++ src/screens/Invite/index.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import Button from 'material-ui/Button'
 import { withStyles } from 'material-ui/styles'
 import { compose } from 'recompose'
+import { reset } from 'redux-form'
 
 import DefaultPage from '../../components/DefaultPage'
 import InviteForm from './InviteForm'
@@ -21,17 +22,19 @@ class Invite extends React.Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
     changePage: PropTypes.func.isRequired,
+    resetForm: PropTypes.func.isRequired,
     match: PropTypes.object.isRequired,
   }
 
   render() {
-    const { changePage, classes, match } = this.props
+    const { changePage, classes, match, resetForm } = this.props
     return (
       <DefaultPage>
         {console.log('Match? ', match)}
         <h1>Invite some people</h1>
         <InviteWithBet
           betId={match.params.id}
+          resetForm={resetForm}
           render={(bet, handleSubmit) => (
             <Fragment>
               <InviteForm participants={bet.participants} onSubmit={handleSubmit} />
@@ -66,11 +69,13 @@ class InviteWithBet extends React.PureComponent {
   }
 
   handleSubmit = async values => {
+    // console.log(this.props.reset)
     await addParticipant(this.props.betId, this.state.bet.participants, values.participant)
     this.setState({
       ...this.state,
       bet: { ...this.state.bet, participants: [...this.state.bet.participants, { id: values.participant }] },
     })
+    this.props.resetForm('InviteForm')
   }
 
   render() {
@@ -84,6 +89,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   changePage: push,
+  resetForm: reset,
 }
 
 const enhance = compose(withStyles(styles), connect(mapStateToProps, mapDispatchToProps))
