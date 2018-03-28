c00cb6134b8fcb7de33eab05aae27a1c6d8607bd
FlorinSenoner <florin.senoner@gmail.com>
2018-03-28 12:49:38 +0200

    add Link to bet view page

--- src/screens/Dashboard/BetList/BetCard.js
+++ src/screens/Dashboard/BetList/BetCard.js
@@ -8,6 +8,8 @@ import { format } from 'date-fns'
 import MoreVertIcon from 'material-ui-icons/MoreVert'
 import Avatar from 'material-ui/Avatar'
 import Menu, { MenuItem } from 'material-ui/Menu'
+import { Link } from 'react-router-dom'
+import Button from 'material-ui/Button'
 
 import { propTypesBet } from '../../../customPropTypes'
 import { deleteBet } from '../../../services/bet.service'
@@ -49,24 +51,26 @@ class BetCard extends PureComponent {
     const { bet, classes } = this.props
     return (
       <Card component="li" className={classes.card} elevation={4}>
-        <CardHeader
-          avatar={bet.title && <Avatar aria-label="Recipe">{bet.title.charAt(0).toUpperCase()}</Avatar>}
-          action={
-            <IconButton aria-owns={anchorEl ? 'simple-menu' : null} aria-haspopup="true" onClick={this.handleClick}>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          component="h3"
-          title={bet.title}
-          subheader={`created: ${format(bet.dateCreated, 'dddd, DD MMM, YYYY')}`}
-        />
-        <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
-          <MenuItem onClick={this.handleClose}>Edit</MenuItem>
-          <MenuItem onClick={this.deleteBet} className={classes.deleteBet}>
-            Delete
-          </MenuItem>
-        </Menu>
-        {/*<CardContent>*/}
+        <Button component={Link} to={`/bet/${bet.id}/view`} variant="raised">
+          <CardHeader
+            avatar={bet.title && <Avatar aria-label="Recipe">{bet.title.charAt(0).toUpperCase()}</Avatar>}
+            action={
+              <IconButton aria-owns={anchorEl ? 'simple-menu' : null} aria-haspopup="true" onClick={this.handleClick}>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            component="h3"
+            title={bet.title}
+            subheader={`created: ${format(bet.dateCreated, 'dddd, DD MMM, YYYY')}`}
+          />
+          <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
+            <MenuItem onClick={this.handleClose}>Edit</MenuItem>
+            <MenuItem onClick={this.deleteBet} className={classes.deleteBet}>
+              Delete
+            </MenuItem>
+          </Menu>
+          {/*<CardContent>*/}
+        </Button>
       </Card>
     )
   }
