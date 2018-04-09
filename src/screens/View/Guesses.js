9e3b1fa839c6ead02c45bc3be6ac3f783ca9bdae
Dennis <dennisbriner@gmail.com>
2018-04-09 12:10:58 +0200

    currently logged in user is marked with a circle when displaying guesses in bet view

--- src/screens/View/Guesses.js
+++ src/screens/View/Guesses.js
@@ -1,21 +1,31 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { compose } from 'recompose'
+import { withStyles } from 'material-ui/styles'
 import List, { ListItem, ListItemText } from 'material-ui/List'
 import Avatar from 'material-ui/Avatar'
 import Divider from 'material-ui/Divider'
 
 import { propTypesUser } from '../../customPropTypes'
 
+const styles = () => ({
+  userAvatarCurrent: {
+    border: '4px turquoise solid',
+  },
+})
+
 class Guesses extends PureComponent {
   showGuess = user => {
-    if (this.props.showAllGuesses || user.uid === this.props.currentUid) {
+    if (this.props.showAllGuesses || this.isCurrentUser(user.uid)) {
       return user.participations[this.props.betId] || 'not guessed yet'
     }
     return undefined
   }
 
+  isCurrentUser = userId => this.props.currentUid === userId
+
   render() {
-    const { users } = this.props
+    const { users, classes } = this.props
 
     return (
       <List>
@@ -23,7 +33,11 @@ class Guesses extends PureComponent {
           users
             .map(user => (
               <ListItem key={user.uid} dense>
-                <Avatar alt="User profile image" src={user.photoURL || '/img/unknown_50x50.jpg'} />
+                <Avatar
+                  className={this.isCurrentUser(user.uid) ? classes.userAvatarCurrent : undefined}
+                  alt="User profile image"
+                  src={user.photoURL || '/img/unknown_50x50.jpg'}
+                />
                 <ListItemText primary={user.displayName} secondary={this.showGuess(user)} />
               </ListItem>
             ))
@@ -46,10 +60,12 @@ class Guesses extends PureComponent {
 }
 
 Guesses.propTypes = {
+  classes: PropTypes.object.isRequired,
   users: PropTypes.arrayOf(propTypesUser).isRequired,
   betId: PropTypes.string.isRequired,
   currentUid: PropTypes.string.isRequired,
   showAllGuesses: PropTypes.bool.isRequired,
 }
+const enhance = compose(withStyles(styles))
 
-export default Guesses
+export default enhance(Guesses)
