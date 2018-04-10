4671a92cd07ad7088346cc943630bcd9de1b4f3e
FlorinSenoner <florin.senoner@gmail.com>
2018-04-10 21:59:44 +0200

    redirect to dashboard on remove bet, replace old history entry

--- src/screens/App/SnackBar/index.js
+++ src/screens/App/SnackBar/index.js
@@ -7,15 +7,19 @@ import Button from 'material-ui/Button'
 import Snackbar from 'material-ui/Snackbar'
 import IconButton from 'material-ui/IconButton'
 import Close from 'material-ui-icons/Close'
+import { replace } from 'react-router-redux'
 
 import { deleteBet } from '../../../services/bet.service'
-import { closeSnackbar } from './actions'
+import { closeSnackbar, openSnackbar } from './actions'
 import { isOpenSelector, textSelector, betIdSelector } from './selectors'
 
 const styles = theme => ({
   close: {
     width: theme.spacing.unit * 4,
     height: theme.spacing.unit * 4,
+  },
+  undoHidden: {
+    display: 'none',
   },
 })
 
@@ -25,6 +29,8 @@ class SnackBar extends Component {
     text: PropTypes.string.isRequired,
     betId: PropTypes.string.isRequired,
     close: PropTypes.func.isRequired,
+    open: PropTypes.func.isRequired,
+    changePage: PropTypes.func.isRequired,
     classes: PropTypes.object.isRequired,
   }
 
@@ -32,6 +38,8 @@ class SnackBar extends Component {
     deleteBet(this.props.betId)
     this.props.close()
     console.log(`Delete bet with Id: ${this.props.betId}`)
+    this.props.open({ text: 'bet has been successfully removed', betId: '' })
+    this.props.changePage('/')
   }
 
   render() {
@@ -50,7 +58,13 @@ class SnackBar extends Component {
         }}
         message={<span id="message-id">{text}</span>}
         action={[
-          <Button key="undo" color="secondary" size="small" onClick={this.clickHandeler}>
+          <Button
+            key="undo"
+            color="secondary"
+            size="small"
+            onClick={this.clickHandeler}
+            className={this.props.betId || classes.undoHidden}
+          >
             UNDO
           </Button>,
           <IconButton key="close" aria-label="Close" color="inherit" className={classes.close} onClick={close}>
@@ -70,6 +84,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   close: closeSnackbar,
+  open: openSnackbar,
+  changePage: replace,
 }
 
 const enhance = compose(connect(mapStateToProps, mapDispatchToProps), withStyles(styles))
