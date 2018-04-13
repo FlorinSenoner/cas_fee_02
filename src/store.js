f0fb73117b1ec3f2e85b67b57f5ba45921aec6f4
Dennis <dennisbriner@gmail.com>
2018-04-13 12:06:52 +0200

    remove currently unused libraries (thunk, date-fns)

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './reducer'
 
@@ -8,7 +7,7 @@ export const history = createHistory()
 
 const initialState = {}
 const enhancers = []
-const middleware = [thunk, routerMiddleware(history)]
+const middleware = [routerMiddleware(history)]
 
 if (process.env.NODE_ENV === 'development') {
   const { devToolsExtension } = window
