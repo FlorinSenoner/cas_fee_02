cc6d5365b1dc4b1c961b9e66fd168ec1ba35c07d
Dennis <dennisbriner@gmail.com>
2018-04-20 11:06:49 +0200

    result evaluation not case sensitive

--- src/screens/View/Result.js
+++ src/screens/View/Result.js
@@ -5,7 +5,9 @@ import ListItemIcon from 'material-ui/List'
 import CorrectIcon from 'material-ui-icons/Check'
 import WrongIcon from 'material-ui-icons/SentimentVeryDissatisfied'
 
-const Result = ({ result, guess }) => <ListItemIcon>{result === guess ? <CorrectIcon /> : <WrongIcon />}</ListItemIcon>
+const Result = ({ result, guess }) => (
+  <ListItemIcon>{result.toLowerCase() === guess.toLowerCase() ? <CorrectIcon /> : <WrongIcon />}</ListItemIcon>
+)
 
 Result.propTypes = {
   result: PropTypes.string.isRequired,
