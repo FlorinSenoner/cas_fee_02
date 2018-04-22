9e8688586d385b902dfb7b59a853a2667e026ef2
Florin Senoner <florin.senoner@gmail.com>
2018-04-22 18:29:50 +0200

    remove dead code

--- src/screens/App/__tests__/login.e2e.test.js
+++ src/screens/App/__tests__/login.e2e.test.js
@@ -1,20 +1,3 @@
-// import React from 'react'
-// import { mountWithRouter } from '../../../utils'
-//
-// import App from '../index'
-//
-// const renderedComponent = mountWithRouter(<App />)
-//
-// describe('<App />', () => {
-//   xit('renders without crashing', () => {
-//     console.log(renderedComponent.debug())
-//     expect(renderedComponent.find('App').length).toBe(1)
-//   })
-//   xit('renders an App', () => {
-//     expect(renderedComponent).toMatchSnapshot()
-//   })
-// })
-
 import puppeteer from 'puppeteer'
 import faker from 'faker'
 import format from 'date-fns/format'
--- src/utils.js
+++ src/utils.js
@@ -31,25 +31,3 @@ export const mountWithRouter = component =>
     .find('MemoryRouter')
     .children()
     .children()
-
-// helpers for puppeteer since wait for navigation is not working
-//
-// export const getLocation = async page => page.evaluate(() => location)
-//
-// export const getLocationProp = async (page, prop) => (await getLocation(page))[prop]
-//
-// const getHistory = async page => page._client.send('Page.getNavigationHistory')
-// const getHistoryEntry = async (page, index) => (await getHistory(page)).entries[index]
-// const getCurrentHistoryEntry = async page => {
-//   const { entries, currentIndex } = await getHistory(page)
-//   return entries[currentIndex]
-// }
-//
-//
-// const pathname = await getLocationProp(page, 'pathname')
-// const re = /(?<=bet\/)(.*)(?=\/invite)/
-// const betId = pathname.match(re)[1]
-// console.log(`A new bet with id: ${betId} was successfully created`)
-//
-// await page.goto('http://localhost:3000/')
-// await page.goto(`http://localhost:3000/bet/${betId}/invites`)
